0c35c7ac4e5ba2336a8e50fd7ed6a902233bf667
Joshua794 <j.england22@outlook.com>
2025-12-04 18:02:25 +0000

    demo

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 import sys
-from pathlib git import pathlib
+from pathlib import Path
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
@@ -7,7 +7,7 @@ sys.path.insert(0, str(root / "src"))
 from app import add
 
 def test_add():
-    assert add(5+6) == 11
+    assert add(5, 6) == 11
 
 def test_add2():
-    assert add(5+6) != 10
+    assert add(5, 6) != 10
