c699b3ab50d50bcdfb80fcc2917f69c0889fde02
Joshua794 <j.england22@outlook.com>
2025-12-10 18:07:51 -0600

    Enhance test_app.py with additional operations
    Added tests for subtraction and commutativity of addition.

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,22 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add
+from app import add, sub, mult, div
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_add2():
+    assert add(5, 6) == add(6, 5)
+
+def test_sub():
+    assert sub(10, 6) == 4
+
+def test_sub2()
+    assert sub(10, 6) != 3
+
+def test_sub3()
+    assert sub(10, 6) == -1 * sub(6, 10)
