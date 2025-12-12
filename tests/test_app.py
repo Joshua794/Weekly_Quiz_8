f75c64ec62d53761e1db9224bb99fb829aa3d843
Joshua794 <j.england22@outlook.com>
2025-12-12 15:56:29 -0600

    Import more functions from app module
    Added additional mathematical functions to the import.

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add, sub, mult, div
+from app import add, sub, mult, div, log, square, sin, cos, sqrt, percentage
 
 # ---BASIC FUNCTIONS--- #
 # ---ADD--- #
